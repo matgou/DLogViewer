54bbe29e267e7319f08723ba2fef8dc96f75ee95
Mathieu GOULIN <mathieu.goulin@gadz.org>
2018-02-25 19:05:47 +0100

    Add linux agent

---
+++ DLogViewer-agent-linux/handler.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# This script handle websocket connection
+# read on stdin : key, command and extra parameters
+
+AgentDirectory=$( dirname $0 )
+LogFile=$AgentDirectory/$LogFile
+configFile=$AgentDirectory/config.txt
+
+. $configFile
+
+# Read filename and histoSize on STDIN
+# tail this logfile
+cmdTail() {
+    read filename
+    log "filename=$filename"
+    read histoSize
+    log "histoSize=$histoSize"
+
+    tail -n $histoSize $filename | while read line;
+    do 
+        echo $line
+        cat /dev/null
+        sync
+    done
+    tail -n 0 -f $filename
+}
+
+# list available file
+cmdLs() {
+    for patern in $PATH_PATERN
+    do
+      log "ls $patern"
+      ls -tr $patern
+    done
+}
+
+# log
+log() {
+ echo $( date "+%Y/%m/%d %H:%M:%S" ) $* >> $LogFile
+}
+
+# Main
+log "Lancement"
+read key
+if [ "$key" != $AGENT_KEY ]
+then
+	log "invalid key : $key"
+	echo "invalid key : $key"
+
+	exit 1
+fi
+
+read cmd
+case $cmd in
+  "ls")
+    cmdLs	
+  ;;
+  "tail")
+    cmdTail
+  ;;  
+esac
+
+exit 0
